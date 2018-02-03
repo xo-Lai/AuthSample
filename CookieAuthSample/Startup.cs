05fe08036045b9c304c51358dd25d0ee05e7ab2c
lai <867777944@qq.com>
2018-02-03 15:06:29 +0800

    Add IdentityServer EntityFramework

--- CookieAuthSample/Startup.cs
+++ CookieAuthSample/Startup.cs
@@ -13,6 +13,10 @@ using CookieAuthSample.Models;
 using Microsoft.AspNetCore.Identity;
 using CookieAuthSample.Services;
 using IdentityServer4.Services;
+using IdentityServer4.EntityFramework;
+using IdentityServer4.EntityFramework.DbContexts;
+using System.Reflection;
+using IdentityServer4.EntityFramework.Mappers;
 
 namespace CookieAuthSample
 {
@@ -28,6 +32,8 @@ namespace CookieAuthSample
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            const string connectionString = "Server=.;Database=aspnet-IdentityServer;User Id=sa;Password=sa;";
+            var migrationAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
             services.AddDbContext<ApplicationDbContext>(option =>
             {
                 option.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
@@ -36,14 +42,19 @@ namespace CookieAuthSample
             services.AddIdentity<ApplicationUser, ApplicationUserRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                .AddDefaultTokenProviders();
-
+            
             services.AddIdentityServer()
                 .AddDeveloperSigningCredential()
-                .AddInMemoryClients(Config.GetClients())
-                .AddInMemoryApiResources(Config.GetApiResources())
-                .AddInMemoryIdentityResources(Config.GetIdentityResources())
-              .AddAspNetIdentity<ApplicationUser>()
-              .Services.AddScoped<IProfileService, ProfileService>();
+                //.AddInMemoryClients(Config.GetClients())
+                //.AddInMemoryApiResources(Config.GetApiResources())
+                //.AddInMemoryIdentityResources(Config.GetIdentityResources())
+              .AddConfigurationStore(options =>
+              options.ConfigureDbContext = builder => builder.UseSqlServer(connectionString, sql => sql.MigrationsAssembly(migrationAssembly)))
+              .AddOperationalStore(options =>
+              options.ConfigureDbContext = builder => builder.UseSqlServer(connectionString, sql => sql.MigrationsAssembly(migrationAssembly)))
+                .AddAspNetIdentity<ApplicationUser>()
+
+              .Services.AddTransient<IProfileService, ProfileService>();
 
 
 
@@ -79,6 +90,8 @@ namespace CookieAuthSample
 
             app.UseStaticFiles();
             app.UseIdentityServer();
+
+            InitIdentityServerDatabase(app);
             //app.UseAuthentication();
             app.UseMvc(routes =>
             {
@@ -86,6 +99,43 @@ namespace CookieAuthSample
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+        }
+
+        public void InitIdentityServerDatabase(IApplicationBuilder app)
+        {
+            using (var scope=app.ApplicationServices.CreateScope())//获取所有的依赖
+            {
+                scope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+                var congifurationDbContext = scope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+
+                if (!congifurationDbContext.Clients.Any())
+                {
+                    foreach (var client in Config.GetClients())
+                    {
+                        congifurationDbContext.Clients.Add(client.ToEntity());
+                    }
+                    congifurationDbContext.SaveChanges();
+                }
+
+                if (!congifurationDbContext.ApiResources.Any())
+                {
+                    foreach (var api in Config.GetApiResources())
+                    {
+                        congifurationDbContext.ApiResources.Add(api.ToEntity());
+                    }
+                    congifurationDbContext.SaveChanges();
+                }
+
+                if (!congifurationDbContext.IdentityResources.Any())
+                {
+                    foreach (var identity in Config.GetIdentityResources())
+                    {
+                        congifurationDbContext.IdentityResources.Add(identity.ToEntity());
+                    }
+                    congifurationDbContext.SaveChanges();
+                }
+            }
         }
     }
 }
